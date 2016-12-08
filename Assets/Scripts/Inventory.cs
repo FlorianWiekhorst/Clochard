c2a91e9fa52fc899634f8d2d32785bd00e266f3c
Turing1887 <tim.zschage@t-online.de>
2016-12-08 21:28:19 +0100

    Added Inventory without functions
    * you can toggle the inventory with "i"

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+	public int space = 10;
+	public int[] items = new int[10];
+	public GameObject inventory;
+	private bool isShowing = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("i")) {
+			showInventory ();
+		} 
+	}
+
+
+	void showInventory(){
+		if (isShowing) {
+			inventory.GetComponent<CanvasGroup> ().alpha = 0f;
+			isShowing = false;
+		} else {
+			inventory.GetComponent<CanvasGroup> ().alpha = 1f;
+			isShowing = true;
+		}
+	
+	}
+
+
+}
