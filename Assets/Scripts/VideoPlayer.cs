cbcc4f302fe372e752966a20c814b2995af7fcc7
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2016-12-12 16:47:34 +0100

    added arcade_automat Peculiar
    + texture & Videotexture
    + Script "VideoPlayer.cs"

---
+++ Assets/Scripts/VideoPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlayer : MonoBehaviour {
+
+	void Start () {
+		MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		movie.Play ();
+		movie.loop = true;
+	}
+}
