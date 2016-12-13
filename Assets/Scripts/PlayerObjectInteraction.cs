22c887eec60f3eeb4e17f2fbb38ed996fc4269eb
Turing1887 <tim.zschage@t-online.de>
2016-12-13 22:09:42 +0100

    Created ma own scene n shit

--- Assets/Scripts/PlayerObjectInteraction.cs
+++ Assets/Scripts/PlayerObjectInteraction.cs
@@ -9,22 +9,27 @@ public class PlayerObjectInteraction : MonoBehaviour {
 	private Vector3 rayOrigin;
 	private RaycastHit hit;
 	public bool isItem;
+	public GameObject interactText;
 
 	void Start () {
 		fpsCam = GetComponent<Camera>();
 		rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3(0.5f,0.5f,0.0f));
+		//rayOrigin = rayOrigin + ( fpsCam.transform.forward * 0.5f ); 
 	}
 
 	void Update () {
-		if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, 40.0f))
+		if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, 30.0f))
 		{
-			if (hit.transform.tag == "Item") {
+			Debug.DrawRay (fpsCam.transform.position,fpsCam.transform.forward,Color.green);
+			if (hit.transform.CompareTag("Item")) {
+				interactText.SetActive (true);
 				isItem = true;
 				if (Input.GetKeyDown ("e")) {
 					Destroy (hit.transform.gameObject);
 				}
 				print ("Works" + fpsCam.transform.position);
 			} else {
+				interactText.SetActive (false);
 				isItem = false;
 			}
 		}
