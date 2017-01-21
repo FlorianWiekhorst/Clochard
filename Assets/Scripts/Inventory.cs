5d70fab6dee559b6d6e004b9a8ffa7d408a65e3e
Turing1887 <tim.zschage@t-online.de>
2017-01-21 17:35:13 +0100

    Script cleanup

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -22,5 +22,9 @@ public class Inventory : MonoBehaviour {
 	
 	}
 
+	public void addItem(){
+		
+	}
+
 
 }
--- Assets/Scripts/PlayerObjectInteraction.cs
+++ Assets/Scripts/PlayerObjectInteraction.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class PlayerObjectInteraction : MonoBehaviour {
 
 
-	private Camera fpsCam;                                              
-	private LineRenderer laserLine;                                     
+	private Camera fpsCam;                                                                      
 	private Vector3 rayOrigin;
 	private RaycastHit hit;
 	public bool isItem;
@@ -17,29 +16,34 @@ public class PlayerObjectInteraction : MonoBehaviour {
 	void Start () {
 		fpsCam = GetComponent<Camera>();
 		rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3(0.5f,0.5f,0.0f));
-		//rayOrigin = rayOrigin + ( fpsCam.transform.forward * 0.5f ); 
 	}
 
 	void Update () {
 		Debug.DrawRay (this.transform.position,fpsCam.transform.forward * distanceToSee,Color.green);
-		if (Physics.Raycast (this.transform.position, fpsCam.transform.forward, out hit,distanceToSee))
-		{
-			
-			Debug.Log ("I hit " + hit.collider.gameObject);
-			if (hit.transform.CompareTag("Item")) {
-				interactText.SetActive (true);
-				isItem = true;
-				if (Input.GetKeyDown ("e")) {
-					Destroy (hit.transform.gameObject);
-					interactText.SetActive (false);
-					isItem = false;
-				}
-				//print ("Works" + fpsCam.transform.position);
-			} else {
-				interactText.SetActive (false);
-				isItem = false;
-			}
+		if (Physics.Raycast (this.transform.position, fpsCam.transform.forward, out hit, distanceToSee)) {
+			itemInteraction ();
+		} else {
+			interactText.SetActive (false);
 		}
 
 	}
+
+	void itemInteraction(){
+
+		Debug.Log ("I hit " + hit.collider.gameObject);
+		if (hit.transform.CompareTag("Item")) {
+			interactText.SetActive (true);
+			isItem = true;
+			if (Input.GetKeyDown ("e")) {
+				Destroy (hit.transform.gameObject);
+				interactText.SetActive (false);
+				isItem = false;
+			}
+		} else {
+			interactText.SetActive (false);
+			isItem = false;
+		}
+	
+	}
+
 }
