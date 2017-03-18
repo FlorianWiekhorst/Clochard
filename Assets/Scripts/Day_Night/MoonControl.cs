646b7b4b4293e7bb2f45f6bd4ab649542b87256e
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2017-03-18 18:31:39 +0100

    berge im Terrain + dayNightScript angefangen

---
+++ Assets/Scripts/Day_Night/MoonControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonControl : MonoBehaviour {
+
+	public float distance = 1000.0f;
+	public float scale = 15.0f;
+
+	void Start () {
+		transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y, distance);
+		transform.localScale = new Vector3 (scale, scale, scale);
+	}
+	
+}
