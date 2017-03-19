c37d9a290a47fd40e014cf21bdca7c4cdb35df60
Turing1887 <tim.zschage@t-online.de>
2017-03-19 16:42:24 +0100

    missions skripte angefangen

---
+++ Assets/Scripts/Story/Mission.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Mission : ScriptableObject {
+
+	public string name;
+	public string description;
+	public string location;
+
+}
