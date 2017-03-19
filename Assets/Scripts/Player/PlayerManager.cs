af0e0a1d791f09e37afe3677b7aae2fa4b5f345e
Turing1887 <tim.zschage@t-online.de>
2017-03-19 22:04:11 +0100

    update PlayerHUD + player manager script
    * stamina implemented

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerManager : MonoBehaviour {
+
+	public int health;
+	public float stamina;
+	public Text staminaText;
+	public Vector3 cacheSpeed;
+	public CharacterController playerController;
+
+
+	void Start(){
+		playerController = GetComponent<CharacterController> ();
+		staminaText.text = stamina.ToString ();
+		cacheSpeed = playerController.velocity;
+	}
+
+	void Update(){
+		if(health == 0){
+			GameOver ();
+		}
+
+
+		if(Input.GetButton("Run")){
+			stamina = Mathf.Clamp(stamina-(Time.deltaTime*10),0,100);
+			staminaText.text = stamina.ToString ("N0");
+			if(stamina == 0.0f){
+				playerController.enabled = false;
+				Invoke ("ResetStamina",3.0f);
+			}
+		}
+	}
+
+	void GameOver(){
+		Debug.Log ("GameOver");
+	}
+	void ResetStamina(){
+		stamina = 100.0f;
+		staminaText.text = stamina.ToString("N0");
+		playerController.enabled = true;
+	}
+
+}
---
+++ Assets/Scripts/Player/StandardToMono.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using Random = UnityEngine.Random;
+
+namespace UnityStandardAssets{
+
+	public class StandardToMono : MonoBehaviour {
+
+		public GameObject player;
+		public FirstPersonController controller;
+
+		// Use this for initialization
+		void Start () {
+			player = GameObject.Find ("_Player");
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
