9364e2156ae3e44e081082b126c945f866b61267
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2017-03-21 17:28:47 +0100

    updated stuff :
    - deleted old tanker
    - added theke in supermarket
    - added Destroyer + Script !

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+	/*	Destroys any Object with tag = "Item" on collision.
+		Destroyer is placed a few meters under the ocean, 
+		to remove Items that dropped of the island. 
+	*/
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Item")) {
+			Destroy(other.gameObject);
+		}
+	}
+}
