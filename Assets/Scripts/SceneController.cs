3cb6e71194718a8a10b96fd0dda66b65a6b21254
Turing1887 <tim.zschage@t-online.de>
2017-03-26 22:25:39 +0200

    Added scenecontroller + changed escMenu alpha to 0

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections.Generic; 
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public class SceneController : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
