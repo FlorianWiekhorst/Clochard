c11d756fddb1a3d84bd84571b415f38737878f6b
1986cisco <cisco-sv@gmx.de>
2017-03-31 22:07:02 +0200

    In der Bar läuft jetzt der preisgekrönte Film '#Frühling'

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovie : MonoBehaviour {
+
+    void Start () {
+        
+            
+       Renderer r = GetComponent<Renderer>();
+       MovieTexture movie = (MovieTexture)r.material.mainTexture;
+       AudioSource aud = GetComponent<AudioSource>();
+       //aud.clip = movie.audioClip;
+
+        movie.Play();
+        movie.loop = true;
+        aud.Play();
+      }
+}
+
