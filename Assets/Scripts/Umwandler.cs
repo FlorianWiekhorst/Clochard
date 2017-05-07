bf5ecb99e1d0f0af845dfb99704f0f813ecd2a01
1986cisco <cisco-sv@gmx.de>
2017-05-07 22:59:04 +0200

    Das Script, was Menschen zerplatzen und in andere Objekte verwandelt, ist fertig. Mit Sound Unterstützung.

---
+++ Assets/Scripts/Umwandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Dieses Script zerstört bei Collision mit dem Spieler das Objekt und wandelt es in andere Objekte um. Dabei ertönt ein Sound
+public class Umwandler : MonoBehaviour {
+
+    public GameObject[] MyPrefabs;
+    public AudioClip MySound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(MySound, transform.position);
+
+            for (int i=0; i<MyPrefabs.Length; i++) {
+                Instantiate(MyPrefabs[i], transform.position, Quaternion.identity);
+            }
+            
+            Debug.Log("You Hit Me");
+            Destroy(gameObject);
+
+        }
+
+    }
+
+   
+}
