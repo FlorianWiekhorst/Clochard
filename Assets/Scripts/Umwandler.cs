360f8e8c741cfc7e7d09b47ab0c8b896d0f2c0cc
1986cisco <cisco-sv@gmx.de>
2017-05-07 23:29:46 +0200

    Man kann dem zu zerplatzendem Objekt jetzt ein Array an Sounds zuweisen, die randommäßig abgespielt werden. Manchmal fallen die durch instantiate generierten Objekte jedoch durch den Boden und fallen ins Endlose. Eventuell könnte man eine unsichtbare Fläche unter der Insel bauen, die alles was runterfällt zerstört.

--- Assets/Scripts/Umwandler.cs
+++ Assets/Scripts/Umwandler.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 
 //Dieses Script zerstört bei Collision mit dem Spieler das Objekt und wandelt es in andere Objekte um. Dabei ertönt ein Sound
+//Manchmal fallen Objekte durch den Boden endlos in die Tiefe
 public class Umwandler : MonoBehaviour {
 
     public GameObject[] MyPrefabs;
-    public AudioClip MySound;
+    public AudioClip[] MySounds;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +24,11 @@ public class Umwandler : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player")
         {
-            AudioSource.PlayClipAtPoint(MySound, transform.position);
+
+            int randomSoundIndex = Random.Range(0, MySounds.Length);
+            AudioSource.PlayClipAtPoint(MySounds[randomSoundIndex], transform.position);
+  
+            
 
             for (int i=0; i<MyPrefabs.Length; i++) {
                 Instantiate(MyPrefabs[i], transform.position, Quaternion.identity);
